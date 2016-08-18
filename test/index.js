e9fa979edb6f8a064b40e6dfbea7148a0d88cdec
Ron Waldon <jokeyrhyme@gmail.com>
2016-08-18 12:35:37 +1000

    FlowType: fix warnings

--- test/index.js
+++ test/index.js
@@ -11,18 +11,18 @@ const PROJECT_PATH = path.join(__dirname, '..')
 
 test('exports a function', (t) => t.is(typeof argvAutoGlob, 'function'))
 
-test('argvAutoGlob() returns Array', (t) => {
-  t.truthy(Array.isArray(argvAutoGlob()))
+test('argvAutoGlob([], {}) returns Array', (t) => {
+  t.truthy(Array.isArray(argvAutoGlob([], {})))
 })
 
-test('argvAutoGlob([ "test/**/*.js" ]) is globbed', (t) => {
+test('argvAutoGlob([ "test/**/*.js" ], { cwd }) is globbed', (t) => {
   const result = argvAutoGlob([ 'test/**/*.js' ], { cwd: PROJECT_PATH })
   const expected = [ 'test/index.js' ]
   t.deepEqual(result, expected)
 })
 
-test('argvAutoGlob([ "test/missing.js" ]) not globbed', (t) => {
-  const result = argvAutoGlob([ 'test/missing.js' ])
+test('argvAutoGlob([ "test/missing.js" ], {}) not globbed', (t) => {
+  const result = argvAutoGlob([ 'test/missing.js' ], {})
   const expected = [ 'test/missing.js' ]
   t.deepEqual(result, expected)
 })
